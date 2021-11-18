fd448f0cd288e027ceebdfe853bfc899d2aff834
mohit <mohitkumar.cse19@chitkarauniversity.edu.in>
2021-11-18 11:06:01 +0530

    register done

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React,{useState} from "react";
 import { useHistory } from "react-router";
 import "./Login.css";
+import ExitToAppTwoToneIcon from "@material-ui/icons/ExitToAppTwoTone";
 function Login() {
-    const history=useHistory()
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
   return (
     <div className="login">
       <div className="login_content">
-        <h2>Login</h2>
+        <div className="login_top">
+          <ExitToAppTwoToneIcon className="login_icon"/>
+          <h2>Login</h2>
+        </div>
         <p>Login to this awesome application</p>
         <div className="login_details">
-          <input type="email" placeholder="Email"/>
-          <input type="password" placeholder="Password"/>
+          <input type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
+          <input type="password" placeholder="Password"  onChange={(e)=>setPassword(e.target.value)} />
         </div>
         <button className="user_login_button">LOGIN</button>
-        <p>Don't have an account ? <strong onClick={()=>{history.push("/register")}}>Register</strong></p>
+        <p>
+          Don't have an account ?{" "}
+          <strong
+            onClick={() => {
+              history.push("/register");
+            }}
+          >
+            Register
+          </strong>
+        </p>
       </div>
     </div>
   );
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import ExitToAppTwoToneIcon from "@material-ui/icons/ExitToAppTwoTone";
+import "./Register.css";
+import { useHistory } from "react-router";
 function Register() {
-    return (
-        <div>
-            <h1>Register</h1>
+    const history=useHistory();
+  return (
+    <div className="register">
+      <div className="register_content">
+        <div className="register_top">
+          <ExitToAppTwoToneIcon className="register_icon" />
+          <h2>Login</h2>
         </div>
-    )
+        <p>Join me and my friends on this great application!</p>
+        <div className="register_inputs">
+          <input type="text" placeholder="Full name" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Password" />
+        </div>
+        <button className="user_register_button">Register</button>
+        <p>Already have an account? <strong onClick={()=>history.push("/login")}>Login</strong></p>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
